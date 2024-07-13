dbaf838193af0f2a49b493bef44ef3f197d372cf
Antti Kautiainen <antti@kautiainen.com>
2024-07-13 13:27:03 +0300

    feat: library main test suite.
    The library main test suite testing everything necessary
    before deployment is loated.
    new file:   test/getopt.test.mjs
    - The main library testing module

---
+++ test/getopt.test.mjs
@@ -0,0 +1,20 @@
+
+/**
+ * @module @kartagia:Getopt/Long/test
+ * 
+ * The main testing module of the getopt.
+ */
+
+describe.skip(
+    "Library", function () {
+        it.skip("Options", function () {
+
+        })
+        it.skip("StateParser", function () {
+
+        })
+        it.skip("Parser", function () {
+
+        })
+    }
+)
