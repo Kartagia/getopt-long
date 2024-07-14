cf0b11cca7e55227ab43d14321e21fe4a8de2c99
Antti Kautiainen <antti@kautiainen.com>
2024-07-15 00:59:06 +0300

    test: ArrayParseResult
    Added constructor test cases:
    - Testing with empty parameter hash
    - Testing with start index 5
    - Testing with end index 5
    - Testing with error index 5
    - Testing with error index 5, start index 3, and current index 8
    
    modified:   test/parser-state/index.test.mjs
    - test: ArrayParseResult
      - test: constructor.
        - test: Default constructor with empty props
        - test: Constructor with start index 5
        - test: Constructor with error index 5
        - test: Constructor with end index 5
        - test: Constructor with error index 5, start index 3, and
        current index 8

--- test/parser-state/index.test.mjs
+++ test/parser-state/index.test.mjs
@@ -15,7 +15,7 @@ describe("class ArrayParseState", function() {
     describe("Constructor", function() {
         [
             {
-                name: "Default constructor",
+                name: "Default constructor with undefined props",
                 params: undefined,
                 test: (value) => {
                     expect(value).instanceof(ArrayParseResult);
@@ -30,7 +30,97 @@ describe("class ArrayParseState", function() {
                         }
                     )
                 }
+            },
+            {
+                name: "Default constructor with empty props",
+                params: {},
+                test: (value) => {
+                    expect(value).instanceof(ArrayParseResult);
+                    [ ["startIndex"], 0, ["currentIndex", 0], 
+                ["createNewResult", false], ["isEnd", false], 
+            ["isError", false] ].forEach(
+                        ([prop, expectedValue=undefined]) => {
+                            expect(value).have(prop);
+                            if (expectedValue !== undefined) {
+                                expect(value).property(prop, expectedValue);
+                            }
+                        }
+                    )
+                }
+            },
+            {
+                name: "Constructor with start index 5",
+                params: {startIndex: 5},
+                test: (value) => {
+                    expect(value).instanceof(ArrayParseResult);
+                    [ ["startIndex"], 5, ["currentIndex", 5], 
+                ["createNewResult", false], ["isEnd", false], 
+            ["isError", false] ].forEach(
+                        ([prop, expectedValue=undefined]) => {
+                            expect(value).have(prop);
+                            if (expectedValue !== undefined) {
+                                expect(value).property(prop, expectedValue);
+                            }
+                        }
+                    )
+                }
+            },
+            {
+                name: "Constructor with error index 5",
+                params: {errorIndex: 5},
+                test: (value) => {
+                    expect(value).instanceof(ArrayParseResult);
+                    [ ["startIndex"], 0, ["currentIndex", 0], 
+                    ["errorIndex", 5],
+                ["createNewResult", false], ["isEnd", false], 
+            ["isError", true] ].forEach(
+                        ([prop, expectedValue=undefined]) => {
+                            expect(value).have(prop);
+                            if (expectedValue !== undefined) {
+                                expect(value).property(prop, expectedValue);
+                            }
+                        }
+                    )
+                }
+            },
+            {
+                name: "Constructor with end index 5",
+                params: {endIndex: 5},
+                test: (value) => {
+                    expect(value).instanceof(ArrayParseResult);
+                    [ ["startIndex"], 0, ["currentIndex", 0], 
+                    ["endIndex", 5],
+                ["createNewResult", false], ["isEnd", true], 
+            ["isError", false] ].forEach(
+                        ([prop, expectedValue=undefined]) => {
+                            expect(value).have(prop);
+                            if (expectedValue !== undefined) {
+                                expect(value).property(prop, expectedValue);
+                            }
+                        }
+                    )
+                }
+            },
+            {
+                name: "Constructor with error index 5, start index 3 and current index 8",
+                params: {errorIndex: 5, startIndex: 3, currentIndex: 8},
+                test: (value) => {
+                    expect(value).instanceof(ArrayParseResult);
+                    [ ["startIndex"], 3, ["currentIndex", 8], 
+                    ["errorIndex", 5],
+                ["createNewResult", false], ["isEnd", false], 
+            ["isError", true] ].forEach(
+                        ([prop, expectedValue=undefined]) => {
+                            expect(value).have(prop);
+                            if (expectedValue !== undefined) {
+                                expect(value).property(prop, expectedValue);
+                            }
+                        }
+                    )
+                }
             }
+
+
         ].forEach( testCase => {
             it(testCase.name, function() {
                 let result = undefined;
